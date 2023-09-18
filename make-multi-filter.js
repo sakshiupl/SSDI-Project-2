85ea968abcaab208a6f49ec5706bee411db4b8ed
Sravani1915 <64299904+Sravani1915@users.noreply.github.com>
2023-09-18 10:27:41 -0400

    Writing clean code for make-multi-filter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,36 +1,24 @@
 "use strict";
-
-
 function MakeMultiFilter(originalArray) {
   // Create a variable to store the currentArray, initially identical to originalArray.
-  let currentArray = [...originalArray];
-
- 
+  let currentArray = [...originalArray]; 
   function arrayFilterer(filterCriteria, callback) {
-
     if (typeof filterCriteria !== 'function') {
       return currentArray;
     }
-
     // Using filterCriteria to filter the currentArray.
     currentArray = currentArray.filter(filterCriteria);
-
     // If callback is a function, call it with the value of currentArray as an argument.
     if (typeof callback === 'function') {
       callback.call(originalArray, currentArray);
     }
-
-   
     return arrayFilterer;
   }
-
-  
   return arrayFilterer;
 }
 
 const arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
 //Call arrayFilterer1 (with a callback function) to filter out elements not equal to 2
-
 arrayFilterer1(
   function (elem) {
     return elem !== 2; // check if element is not equal to 2
@@ -40,7 +28,6 @@ arrayFilterer1(
     console.log(currentArray); 
   },
 );
-
 // Call arrayFilterer1 (without a callback function) to filter out elements not equal to 3
 const filteredArray = arrayFilterer1(function (elem) {
   return elem !== 3; // check if element is not equal to 3
